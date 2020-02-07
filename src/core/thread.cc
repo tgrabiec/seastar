05228e0cc3e6d5fc403c039a4a23ceecbae325ec
Tomasz Grabiec <tgrabiec@scylladb.com>
2020-02-07 16:11:35 +0100

    core/reactor: Expose a pointer to currently running task
    Will be used by current_backtrace() for backtracking across tasks.

--- src/core/thread.cc
+++ src/core/thread.cc
@@ -227,6 +227,7 @@ thread_context::setup(size_t stack_size) {
 
 void
 thread_context::switch_in() {
+    local_engine->_current_task = nullptr; // thread_wake_task is on the stack and will be invalid when we resume
     _context.switch_in();
 }
 
